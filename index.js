3e6bac6e293b1346599e199f7d932d1e01c8af9c
Jose <joseluis0528@gmail.com>
2023-03-14 15:14:19 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,15 +1,12 @@
 function scuberGreetingForFeet(number) {
   // Write your code here!
-  if (number < 400) {
+  if (number <= 400) {
     return "This one is on me!";
-  }
-  else if (number > 400 && number < 2000 ) {
+  } else if (number > 400 && number < 2000 ) {
     return "That will be twenty bucks.";
-  }
-  else if (number > 2000 && number < 2500) {
+  } else if (number > 2000 && number < 2500) {
     return "I will gladly take your thirty bucks.";
-  }
-  else if (number > 2500) {
+  } else if (number > 2500) {
     return "No can do.";
   }
 }
